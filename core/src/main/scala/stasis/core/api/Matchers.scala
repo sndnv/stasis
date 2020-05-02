f3d9b99ee242ae30efb7a4d684002c8f11fe7ac7
Angel Sanadinov <sndnv@users.noreply.github.com>
2020-05-02 14:00:41 +0200

    [core] Add support for FiniteDuration as an API paramter

--- core/src/main/scala/stasis/core/api/Matchers.scala
+++ core/src/main/scala/stasis/core/api/Matchers.scala
@@ -6,6 +6,9 @@ import java.time.format.DateTimeParseException
 import akka.http.scaladsl.server.{PathMatcher, PathMatcher1}
 import akka.http.scaladsl.unmarshalling.Unmarshaller
 
+import scala.concurrent.Future
+import scala.concurrent.duration.{Duration, FiniteDuration}
+
 object Matchers {
   val IsoInstant: PathMatcher1[Instant] = {
     PathMatcher("""\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(?:\.\d+)?Z""".r)
@@ -17,4 +20,12 @@ object Matchers {
 
   implicit val stringToInstant: Unmarshaller[String, Instant] =
     Unmarshaller.strict(Instant.parse)
+
+  implicit val stringToFiniteDuration: Unmarshaller[String, FiniteDuration] =
+    Unmarshaller { _ => string =>
+      Duration(string) match {
+        case duration: FiniteDuration => Future.successful(duration)
+        case other                    => Future.failed(new IllegalArgumentException(s"Expected FiniteDuration but [$other] provided"))
+      }
+    }
 }
