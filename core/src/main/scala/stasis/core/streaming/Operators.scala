76f79facb4362ac360af7be26eeac1121f147dfc
Angel Sanadinov <sndnv@users.noreply.github.com>
2022-07-12 09:49:13 +0200

    [core] Add streaming operator for dropping latest duplicate messages

--- core/src/main/scala/stasis/core/streaming/Operators.scala
+++ core/src/main/scala/stasis/core/streaming/Operators.scala
@@ -9,6 +9,15 @@ import scala.concurrent.{Future, Promise}
 
 object Operators {
   implicit class ExtendedSource[+Out, +Mat](source: Source[Out, Mat]) {
+
+    /**
+      * Materializes the stream and cancels it, without processing elements.
+      * <br/><br/>
+      * Note: Some elements may still be consumed and processed, depending
+      * on the definition/behaviour of the source.
+      *
+      * @return future that completes when the stream terminates
+      */
     def cancelled()(implicit mat: Materializer): Future[Done] = {
       import mat.executionContext
 
@@ -31,8 +40,46 @@ object Operators {
       promise.future
     }
 
+    /**
+      * Materializes the stream and consumes all elements by ignoring them.
+      *
+      * @return future that completes when the stream terminates
+      */
     def ignored()(implicit mat: Materializer): Future[Done] =
       source.runWith(Sink.ignore)
+
+    /**
+      * Drops elements that have already been sent downstream.
+      * <br/><br/>
+      * Note: Only checks against the last emitted element, if any. For example:
+      * <pre>
+      *
+      *  val result = Source(List("a", "b", "b", "c", "b"))
+      *                 .dropDuplicates(f = Option.apply)
+      *                 .runWith(Sink.seq)
+      *                 .await
+      *
+      *  result == Seq("a", "b", "c", "b")
+      * </pre>
+      *
+      * @return
+      */
+    @SuppressWarnings(Array("org.wartremover.warts.Var", "org.wartremover.warts.Null"))
+    def dropLatestDuplicates[T >: Null](f: Out => Option[T]): Source[T, Mat] =
+      source.statefulMapConcat { () =>
+        var last: T = null
+
+        { element =>
+          f(element).flatMap {
+            case current if current == last =>
+              None
+
+            case current =>
+              last = current
+              Some(current)
+          }.toList
+        }
+      }
   }
 
   implicit class ExtendedByteStringSource[+Mat](source: Source[ByteString, Mat]) {
