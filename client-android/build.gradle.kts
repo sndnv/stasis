5ba15d62a672ec454a007b146a9de7e4e8515e35
Angel Sanadinov <sndnv@users.noreply.github.com>
2022-08-10 22:21:48 +0200

    [client-android] Update dependencies

--- client-android/build.gradle.kts
+++ client-android/build.gradle.kts
@@ -9,10 +9,10 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath("com.squareup.wire:wire-gradle-plugin:4.4.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.1")
     }
 }
 
