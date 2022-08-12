a45243559c65b8b4ef06e70789fae93fa14fffc9
Angel Sanadinov <sndnv@users.noreply.github.com>
2022-08-12 09:23:45 +0200

    [client-android] Update to compress dataset metadata before caching locally

--- client-android/app/src/test/java/stasis/test/client_android/persistence/cache/DatasetMetadataCacheFileSerdesSpec.kt
+++ client-android/app/src/test/java/stasis/test/client_android/persistence/cache/DatasetMetadataCacheFileSerdesSpec.kt
@@ -1,6 +1,7 @@
 package stasis.test.client_android.persistence.cache
 
 import okio.ByteString.Companion.decodeBase64
+import org.hamcrest.CoreMatchers.anyOf
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
@@ -11,20 +12,52 @@ import stasis.client_android.lib.model.DatasetMetadata
 import stasis.client_android.lib.model.FilesystemMetadata
 import stasis.client_android.lib.utils.Try
 import stasis.client_android.persistence.cache.DatasetMetadataCacheFileSerdes
+import stasis.test.client_android.Fixtures
 import java.util.UUID
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Config.OLDEST_SDK])
 class DatasetMetadataCacheFileSerdesSpec {
     private val datasetMetadata = DatasetMetadata(
-        contentChanged = emptyMap(),
-        metadataChanged = emptyMap(),
+        contentChanged = mapOf(
+            Fixtures.Metadata.FileOneMetadata.path to Fixtures.Metadata.FileOneMetadata
+        ),
+        metadataChanged = mapOf(
+            Fixtures.Metadata.FileTwoMetadata.path to Fixtures.Metadata.FileTwoMetadata,
+            Fixtures.Metadata.FileThreeMetadata.path to Fixtures.Metadata.FileThreeMetadata
+        ),
         filesystem = FilesystemMetadata(
-            entities = emptyMap()
+            entities = mapOf(
+                Fixtures.Metadata.FileOneMetadata.path to FilesystemMetadata.EntityState.New,
+                Fixtures.Metadata.FileTwoMetadata.path to FilesystemMetadata.EntityState.New,
+                Fixtures.Metadata.FileThreeMetadata.path to FilesystemMetadata.EntityState.Updated
+            )
         )
     )
 
-    private val serializedDatasetMetadata = "GgA="
+    private val compressedDatasetMetadata11 =
+        "H4sIAAAAAAAAAOJqYuTi1S/JLdBPy8xJ1c/PSxUq5MpH" +
+                "ExJg1GhYMWFx46Mfx/8zGvz/ePXK+lntFlYsRfn5JU" +
+                "5g0ouzqLwCgoIYGaM0uPhRDIg3EBLl2NHa27rj777z" +
+                "RgKnvq/8f2D7vHcbGZNY8kBWTkNxREl5vlALI1cjuq" +
+                "AAkxSSsSUZRampGjDHGCAciNNdWlGayO4Cmgh0lxhH" +
+                "S9vVKw9Xrdr5jFFg188J7f3NS390AR2WXpVZINTByM" +
+                "WH0JGWX1okVMZVgi4mwESSOxiA7hBANQEcQE+bJ66a" +
+                "9XT7mRqBNfOX7plxfOK1qYxJ7CmpaTmJJalSDlzC6D" +
+                "HFxMWAKggKOZCgCIarmaQYAAAAAP//AwAO6eeE6wEA" +
+                "AA=="
+
+    private val compressedDatasetMetadata17 =
+        "H4sIAAAAAAAA/+JqYuTi1S/JLdBPy8xJ1c/PSxUq5MpH" +
+                "ExJg1GhYMWFx46Mfx/8zGvz/ePXK+lntFlYsRfn5JU" +
+                "5g0ouzqLwCgoIYGaM0uPhRDIg3EBLl2NHa27rj777z" +
+                "RgKnvq/8f2D7vHcbGZNY8kBWTkNxREl5vlALI1cjuq" +
+                "AAkxSSsSUZRampGjDHGCAciNNdWlGayO4Cmgh0lxhH" +
+                "S9vVKw9Xrdr5jFFg188J7f3NS390AR2WXpVZINTByM" +
+                "WH0JGWX1okVMZVgi4mwESSOxiA7hBANQEcQE+bJ66a" +
+                "9XT7mRqBNfOX7plxfOK1qYxJ7CmpaTmJJalSDlzC6D" +
+                "HFxMWAKggKOZCgCIarmaQYAAAAAP//AwAO6eeE6wEA" +
+                "AA=="
 
     @Test
     fun serializeKeys() {
@@ -39,15 +72,21 @@ class DatasetMetadataCacheFileSerdesSpec {
     fun serializeValues() {
         assertThat(
             DatasetMetadataCacheFileSerdes.serializeValue(datasetMetadata),
-            equalTo(serializedDatasetMetadata.decodeBase64()?.toByteArray())
+            anyOf(
+                equalTo(compressedDatasetMetadata11.decodeBase64()?.toByteArray()),
+                equalTo(compressedDatasetMetadata17.decodeBase64()?.toByteArray())
+            )
         )
     }
 
     @Test
     fun deserializeValues() {
-        assertThat(
-            DatasetMetadataCacheFileSerdes.deserializeValue(serializedDatasetMetadata.decodeBase64()!!.toByteArray()),
-            equalTo(Try.Success(datasetMetadata))
-        )
+        val deserialized11 =
+            DatasetMetadataCacheFileSerdes.deserializeValue(compressedDatasetMetadata11.decodeBase64()!!.toByteArray())
+        assertThat(deserialized11, equalTo(Try.Success(datasetMetadata)))
+
+        val deserialized17 =
+            DatasetMetadataCacheFileSerdes.deserializeValue(compressedDatasetMetadata17.decodeBase64()!!.toByteArray())
+        assertThat(deserialized17, equalTo(Try.Success(datasetMetadata)))
     }
 }
