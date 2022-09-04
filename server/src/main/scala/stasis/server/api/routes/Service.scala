78d6d40dce900b18e170ef661f91f81627821525
Angel Sanadinov <sndnv@users.noreply.github.com>
2022-09-04 19:48:11 +0200

    [server] Add health-check endpoint

--- server/src/main/scala/stasis/server/api/routes/Service.scala
+++ server/src/main/scala/stasis/server/api/routes/Service.scala
@@ -1,6 +1,7 @@
 package stasis.server.api.routes
 
 import akka.actor.typed.scaladsl.LoggerOps
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import stasis.server.security.CurrentUser
@@ -11,13 +12,18 @@ class Service()(implicit ctx: RoutesContext) extends ApiRoutes {
   import stasis.shared.api.Formats._
 
   def routes(implicit currentUser: CurrentUser): Route =
-    path("ping") {
-      get {
-        val response = Ping()
-        log.debugN("Received ping request from user [{}]; responding with [{}]", currentUser, response.id)
-        discardEntity & complete(response)
+    concat(
+      path("ping") {
+        get {
+          val response = Ping()
+          log.debugN("Received ping request from user [{}]; responding with [{}]", currentUser, response.id)
+          discardEntity & complete(response)
+        }
+      },
+      path("health") {
+        complete(StatusCodes.OK)
       }
-    }
+    )
 }
 
 object Service {
