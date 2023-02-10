e7005a6bc62a1210c1d9bea202f399234232672a
Angel Sanadinov <angel.sanadinov@lunatech.com>
2023-02-10 15:06:53 +0100

    [identity-ui] Update dependencies

--- identity-ui/lib/pages/default/not_found.dart
+++ identity-ui/lib/pages/default/not_found.dart
@@ -14,13 +14,13 @@ class NotFound extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text('404', style: theme.textTheme.headline1),
-            Text('Page Not Found', style: theme.textTheme.headline3),
-            Text('The requested resource could not be found.', style: theme.textTheme.headline6),
+            Text('404', style: theme.textTheme.displayLarge),
+            Text('Page Not Found', style: theme.textTheme.displaySmall),
+            Text('The requested resource could not be found.', style: theme.textTheme.titleLarge),
             RichText(
               text: TextSpan(
                   text: 'Go Home',
-                  style: theme.textTheme.bodyText1?.copyWith(color: theme.primaryColor, fontStyle: FontStyle.italic),
+                  style: theme.textTheme.bodyLarge?.copyWith(color: theme.primaryColor, fontStyle: FontStyle.italic),
                   recognizer: TapGestureRecognizer()
                     ..onTap = () => PageRouter.navigateTo(context, destination: PageRouterDestination.home)),
             ),
