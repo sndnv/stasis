2092183dc4c2be2b4f803e27737d3ec60b68d801
Angel Sanadinov <angel.sanadinov@lunatech.com>
2023-02-10 15:07:52 +0100

    [client-android] Update dependencies

--- client-android/app/build.gradle.kts
+++ client-android/app/build.gradle.kts
@@ -9,13 +9,13 @@ plugins {
 dependencies {
     implementation(project(":lib"))
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
-    implementation("com.google.code.gson:gson:2.10")
-    implementation("com.google.android.material:material:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.21")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.fragment:fragment-ktx:1.5.4")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
@@ -24,11 +24,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("androidx.room:room-runtime:2.4.3")
-    implementation("androidx.room:room-ktx:2.4.3")
+    implementation("androidx.room:room-runtime:2.5.0")
+    implementation("androidx.room:room-ktx:2.5.0")
     implementation("androidx.security:security-crypto:1.1.0-alpha04")
-    implementation("org.bitbucket.b_c:jose4j:0.9.2")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
+    implementation("org.bitbucket.b_c:jose4j:0.9.3")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.21")
     implementation("com.getkeepsafe.taptargetview:taptargetview:1.13.3")
 
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
@@ -36,22 +36,22 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.44.2")
     kapt("com.google.dagger:hilt-compiler:2.44.2")
-    kapt("androidx.room:room-compiler:2.4.3")
+    kapt("androidx.room:room-compiler:2.5.0")
 
-    testImplementation("io.mockk:mockk:1.13.2")
-    testImplementation("org.robolectric:robolectric:4.9")
+    testImplementation("io.mockk:mockk:1.13.3")
+    testImplementation("org.robolectric:robolectric:4.9.2")
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.test:core:1.5.0")
 
-    androidTestImplementation("androidx.test:runner:1.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
     androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-    androidTestImplementation("androidx.room:room-testing:2.4.3")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.room:room-testing:2.5.0")
     androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
     androidTestImplementation("io.mockk:mockk-android:1.13.2")
 
-    debugImplementation("androidx.fragment:fragment-testing:1.5.4") {
+    debugImplementation("androidx.fragment:fragment-testing:1.5.5") {
         exclude(group = "androidx.test", module = "monitor")
     }
 }
--- client-android/build.gradle.kts
+++ client-android/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:7.4.1")
         classpath("com.squareup.wire:wire-gradle-plugin:4.4.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
