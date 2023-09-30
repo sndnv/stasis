5e15478a79aff5a3381f34c4f2ec0f90dfe57aa5
Angel Sanadinov <sndnv@users.noreply.github.com>
2023-09-30 23:52:37 +0200

    [client-cli] Update dependencies

--- client-cli/setup.py
+++ client-cli/setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'requests==2.28.2',
+    'requests==2.31.0',
     'requests-oauthlib==1.3.1',
     'terminaltables==3.1.10',
     'hurry.filesize==0.9',
-    'pyhocon==0.3.59',
-    'click==8.1.3',
-    'pyparsing==2.4.7',
-    'psutil==5.9.4',
-    'cryptography==41.0.2',
-    'sseclient-py==1.7.2',
-    'tqdm==4.64.1',
+    'pyhocon==0.3.60',
+    'click==8.1.7',
+    'pyparsing==3.1.1',
+    'psutil==5.9.5',
+    'cryptography==41.0.4',
+    'sseclient-py==1.8.0',
+    'tqdm==4.66.1',
 ]
 
 tests_require = []
