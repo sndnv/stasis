854c44fcae28f0483edb599180263430cfcd973c
sndnv <sndnv@users.noreply.github.com>
2023-10-22 22:01:33 +0000

    Updating version to 0.0.4

--- client-android/app/build.gradle.kts
+++ client-android/app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
 
         applicationId = "stasis.client.android"
         versionCode = 1
-        versionName = "0.0.4-SNAPSHOT"
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
