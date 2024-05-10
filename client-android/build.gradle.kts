f01ff18725c31dcf95111754c6ff9715943a3202
Angel Sanadinov <angel.sanadinov@lunatech.com>
2024-05-10 12:57:25 +0200

    [client-android] Update dependencies

--- client-android/build.gradle.kts
+++ client-android/build.gradle.kts
@@ -8,11 +8,11 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
         classpath("com.android.tools.build:gradle:8.3.2")
-        classpath("com.squareup.wire:wire-gradle-plugin:4.9.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.3")
+        classpath("com.squareup.wire:wire-gradle-plugin:4.9.9")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     }
 }
 
@@ -24,8 +24,8 @@ allprojects {
 }
 
 plugins {
-    id("io.gitlab.arturbosch.detekt") version "1.19.0"
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
 
 subprojects {
--- client-android/lib/build.gradle.kts
+++ client-android/lib/build.gradle.kts
@@ -8,24 +8,24 @@ plugins {
 }
 
 dependencies {
-    api("com.squareup.wire:wire-runtime:4.9.1")
+    api("com.squareup.wire:wire-runtime:4.9.9")
 
     implementation(kotlin("stdlib"))
-    implementation("com.squareup.okio:okio:3.5.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
-    implementation("com.squareup.moshi:moshi-adapters:1.15.0")
+    implementation("com.squareup.okio:okio:3.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("com.squareup.moshi:moshi-adapters:1.15.1")
     implementation("at.favre.lib:hkdf:2.0.0")
-    implementation("org.bitbucket.b_c:jose4j:0.9.3")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
-    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.0")
+    implementation("org.bitbucket.b_c:jose4j:0.9.6")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.20")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.7.2")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.7.2")
-    testImplementation("io.kotest:kotest-property-jvm:5.7.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.1")
+    testImplementation("io.kotest:kotest-property-jvm:5.8.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     testImplementation("com.google.jimfs:jimfs:1.3.0")
 }
 
