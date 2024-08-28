4268f59cd1d958042722fd8e0c8b79ddfad6b9d7
sndnv <sndnv@users.noreply.github.com>
2024-08-28 21:00:09 +0000

    Updating version to 1.1.1

--- client-android/app/build.gradle.kts
+++ client-android/app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
 
         applicationId = "stasis.client.android"
         versionCode = 1
-        versionName = "1.1.1-SNAPSHOT"
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- client-android/build.gradle.kts
+++ client-android/build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
 }
 
 subprojects {
-    version = "1.1.1-SNAPSHOT"
+    version = "1.1.1"
 }
 
 allprojects {
