fa695b374ec666459e90545bf5003b40434b38e9
Angel Sanadinov <angel.sanadinov@lunatech.com>
2025-02-03 23:24:55 +0100

    [client-android] Rename active "schedules" table to "active_schedules"

--- client-android/app/src/main/java/stasis/client_android/persistence/schedules/ActiveScheduleEntity.kt
+++ client-android/app/src/main/java/stasis/client_android/persistence/schedules/ActiveScheduleEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import stasis.client_android.lib.model.server.schedules.ScheduleId
 
-@Entity(tableName = "schedules")
+@Entity(tableName = "active_schedules")
 data class ActiveScheduleEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
--- client-android/app/src/main/java/stasis/client_android/persistence/schedules/ActiveScheduleEntityDatabase.kt
+++ client-android/app/src/main/java/stasis/client_android/persistence/schedules/ActiveScheduleEntityDatabase.kt
@@ -5,16 +5,25 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import org.jetbrains.annotations.TestOnly
 import stasis.client_android.persistence.Converters
 
-@Database(entities = [ActiveScheduleEntity::class], version = 1, exportSchema = false)
+@Database(entities = [ActiveScheduleEntity::class], version = 2, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class ActiveScheduleEntityDatabase : RoomDatabase() {
     abstract fun dao(): ActiveScheduleEntityDao
 
     companion object {
-        private const val DefaultDatabase: String = "schedules.db"
+        private const val DefaultDatabase: String = "active_schedules.db"
+        private const val PreviousDefaultDatabase: String = "schedules.db"
+
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE schedules RENAME TO active_schedules")
+            }
+        }
 
         @Volatile
         private var INSTANCE: ActiveScheduleEntityDatabase? = null
@@ -34,11 +43,19 @@ abstract class ActiveScheduleEntityDatabase : RoomDatabase() {
                 INSTANCE = instance
             }
 
-        private fun build(context: Context, database: String) =
-            Room.databaseBuilder(
-                context.applicationContext,
-                ActiveScheduleEntityDatabase::class.java,
-                database
-            ).build()
+        private fun build(context: Context, database: String): ActiveScheduleEntityDatabase {
+            context.databaseList().filter { it.startsWith(PreviousDefaultDatabase) }.forEach {
+                val databaseFile = context.getDatabasePath(it)
+                if (databaseFile.exists()) {
+                    val updatedName = databaseFile.name.replace(PreviousDefaultDatabase, DefaultDatabase)
+                    val updatedDatabaseFile = java.io.File(databaseFile.parentFile, updatedName)
+                    databaseFile.renameTo(updatedDatabaseFile)
+                }
+            }
+
+            return Room.databaseBuilder(context.applicationContext, ActiveScheduleEntityDatabase::class.java, database)
+                .addMigrations(MIGRATION_1_2)
+                .build()
+        }
     }
 }
