6399b5efc8126ba1361a933625b15976f4b309cf
Angel Sanadinov <angel.sanadinov@lunatech.com>
2025-06-11 12:39:44 +0200

    [python] Update dependencies

--- client-cli/setup.py
+++ client-cli/setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'requests==2.32.3',
+    'requests==2.32.4',
     'requests-oauthlib==2.0.0',
     'terminaltables==3.1.10',
     'hurry.filesize==0.9',
     'pyhocon==0.3.61',
-    'click==8.1.8',
-    'pyparsing==3.2.1',
+    'click==8.2.1',
+    'pyparsing==3.2.3',
     'psutil==6.1.1',
-    'cryptography==44.0.1',
+    'cryptography==45.0.4',
     'sseclient-py==1.8.0',
     'tqdm==4.67.1',
 ]
