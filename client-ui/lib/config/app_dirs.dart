3e0538f62d13defd56c0c7c2dbcf15c91df38a45
Angel Sanadinov <angel.sanadinov@lunatech.com>
2026-05-15 13:47:06 +0200

    feat(client-ui): Add Windows support

--- client-ui/lib/config/app_dirs.dart
+++ client-ui/lib/config/app_dirs.dart
@@ -1,21 +1,43 @@
 import 'dart:io';
 
 class AppDirs {
-  static String getUserConfigDir({required String applicationName}) {
-    final xdgConfigHome = Platform.environment['XDG_CONFIG_HOME']?.trimTrailingSlash();
+  static String getUserConfigDir({
+    required String applicationName,
+    String? operatingSystem,
+    Map<String, String>? environment,
+  }) {
+    final os = OperatingSystem.from(operatingSystem ?? Platform.operatingSystem);
+    final env = environment ?? Platform.environment;
+    final separator = Platform.pathSeparator;
+    final xdgConfigHome = env['XDG_CONFIG_HOME']?.trimTrailingSlash();
 
     if (xdgConfigHome != null) {
-      return '$xdgConfigHome/$applicationName';
+      return '$xdgConfigHome$separator$applicationName';
     } else {
-      final userHome = Platform.environment['HOME']?.trimTrailingSlash() ?? '~';
-      if (Platform.isLinux) {
-        return '$userHome/.config/$applicationName';
-      } else if (Platform.isMacOS) {
-        return '$userHome/Library/Preferences/$applicationName';
-      } else {
-        throw UnsupportedError('Unsupported operating system: [${Platform.operatingSystem}]');
+      final userHome = env[os == OperatingSystem.windows ? 'USERPROFILE' : 'HOME']?.trimTrailingSlash() ?? '~';
+      switch (os) {
+        case OperatingSystem.linux:
+          return '$userHome/.config/$applicationName';
+        case OperatingSystem.macos:
+          return '$userHome/Library/Preferences/$applicationName';
+        case OperatingSystem.windows:
+          final localAppData = env['LOCALAPPDATA']?.trimTrailingSlash() ?? '$userHome\\AppData\\Local';
+          return '$localAppData\\$applicationName';
       }
     }
+  }
+}
+
+enum OperatingSystem {
+  linux,
+  macos,
+  windows;
+
+  factory OperatingSystem.from(String name) {
+    return OperatingSystem.values.firstWhere(
+      (os) => os.name == name.toLowerCase(),
+      orElse: () => throw UnsupportedError('Unsupported operating system: [$name]'),
+    );
   }
 }
 
